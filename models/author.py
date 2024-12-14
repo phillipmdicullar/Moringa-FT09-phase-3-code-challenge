086fb5869667e22b09b3015f874f79232318c4d1
Philip Emdokolo <phillip.emdokolo@student.moringaschool.com>
2024-12-14 23:00:44 +0300

    created the different required fucntions for author

--- models/author.py
+++ models/author.py
@@ -4,6 +4,23 @@ class Author:
     def __init__(self, id, name):
         self.id = id
         self.name = name
-
-    def __repr__(self):
-        return f'<Author {self.name}>'
+    def id(self):
+        return self.id
+    @property
+    def name(self):
+        return self._name
+    @staticmethod
+    def validate_name(name):
+        return name
+    def save(self):
+        return self
+    pass
+    def articles(self):
+        return cursor.fetchall()
+    def magazines(self):
+        return cursor.fetchall()
+    @classmethod
+    def drop_table(cls):
+    # def __repr__(self):
+    #     return f'<Author {self.name}>'
+    
