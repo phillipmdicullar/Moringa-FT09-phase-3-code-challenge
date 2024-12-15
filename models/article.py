7c54305fdf04512054c438d1b572058f713834e8
Philip Emdokolo <phillip.emdokolo@student.moringaschool.com>
2024-12-15 17:22:59 +0300

    optimised article initialization for clarity and simplicity

--- models/article.py
+++ models/article.py
@@ -1,10 +1,10 @@
 class Article:
     def __init__(self, id, title, content, author_id, magazine_id):
-        self.id = id
-        self.title = title
-        self.content = content
-        self.author_id = author_id
-        self.magazine_id = magazine_id
-
+        self._id = id
+        self._title = title
+        self._content = content
+        self._author_id = author_id
+        self._magazine_id = magazine_id
+        self.save() #
     def __repr__(self):
         return f'<Article {self.title}>'
