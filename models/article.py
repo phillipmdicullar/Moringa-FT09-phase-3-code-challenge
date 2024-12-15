cf05d49bbaed56b9e06e97e6f48c6d13ed01f061
Philip Emdokolo <phillip.emdokolo@student.moringaschool.com>
2024-12-15 17:44:59 +0300

    refining all the code error test cases

--- models/article.py
+++ models/article.py
@@ -1,33 +1,56 @@
+from database.connection import get_db_connection
+from models.author import Author
+from models.magazine import Magazine
+
+
 class Article:
-    def __init__(self, id, title, content, author_id, magazine_id):
-        self._id = id
-        self._title = title
-        self._content = content
-        self._author_id = author_id
-        self._magazine_id = magazine_id
-        self.save() #
-    def __repr__(self):
-        return f'<Article {self.title}>'
+    def __init__(self, title, content, author, magazine):
+        self._id = None
+        self._title = self.validate_title(title)
+        self._content = content  # No need for validation if just a string
+        self._author = author
+        self._magazine = magazine
+        self.save()
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def content(self):
+        return self._content
+
+    @property
+    def author(self):
+        return self._author
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    def save(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO articles (title, content, author_id, magazine_id) 
+            VALUES (?, ?, ?, ?)
+        ''', (self._title, self._content, self._author.id, self._magazine.id))
+        conn.commit()
+        self._id = cursor.lastrowid
+
     @staticmethod
     def validate_title(title):
         if not isinstance(title, str) or not (5 <= len(title) <= 50):
             raise ValueError("Title must be a string between 5 and 50 characters")
         return title
+
     @classmethod
-    def create_table(cls):
+    def drop_table(cls):
         conn = get_db_connection()
         cursor = conn.cursor()
-        cursor.execute('''CREATE TABLE IF EXISTS articles(
-            id INTEGER PRIMARY KEY,
-            title TEXT NOT NULL,
-            content TEXT NOT NULL,
-            author_id INTEGER NOT NULL,
-            magazine_id INTEGER NOT NULL,
-            FOREIGN KEY(author_id) REFERENCES authors(id),
-            FOREIGN KEY(magazine_id) REFERENCES magazines(id))''')
+        cursor.execute('DROP TABLE IF EXISTS articles')
         conn.commit()
-    def save(self):
-        with get_db_connection() as conn:
-            cursor = conn.cursor()
-            cursor.execute('''INSERT INTO articles (title, content, author_id, magazine_id) VALUES (?, ?, ?, ?)''',(self._title, self._content, self._author.id, self._magazine.id))
-            self._id = cursor.lastrowid
